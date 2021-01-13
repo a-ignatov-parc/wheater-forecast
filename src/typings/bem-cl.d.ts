99afe38d41dfae21ef004c62659468bd8615babf
Anton Ignatov <anton.ignatov@project-plato.com>
2021-01-13 10:43:38 +0100

    Fixed incorrect bem-cl types

--- src/typings/bem-cl.d.ts
+++ src/typings/bem-cl.d.ts
@@ -1,5 +1,5 @@
 declare module "bem-cl" {
-  const x: (className: string) => string | { mix: (...args: any) => string };
+  const x: (className: string) => string | { mix: (...args: any[]) => string };
   const bemCl: (blockName: string) => x;
 
   export default bemCl;
